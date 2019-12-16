fe3ab05ef9a2f50b0910cc193cbfd219fe1076e2
KnightsUCF <peterk@knights.ucf.edu>
2019-12-16 18:19:56 -0500

    Create Game.cs

---
+++ Code/Game.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+
+    void Load()
+    {
+        Debug.Log("Loading game...");
+    }
+
+
+
+    void Awake()
+    {
+        Load();
+    }
+
+
+}
