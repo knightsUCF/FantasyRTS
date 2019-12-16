2e62fca0d0b1d215dab26eadf3195ccac64d7ad4
KnightsUCF <peterk@knights.ucf.edu>
2019-12-16 18:20:07 -0500

    Create HP.cs

---
+++ Code/HP.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class HP : MonoBehaviour
+{
+
+    // R E F E R E N C E S //////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+    public GameObject parentObjectToDestroy;
+    Settings settings;
+
+
+    
+    // G L O B A L S ////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    public int hp;
+
+
+
+    // M E T H O D S ////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    public void UpdateHP(int amount)
+    {
+        hp = amount;
+        GetComponent<TextMesh>().text = hp.ToString();
+
+        if (hp <= 0)
+        {
+            // if (GetComponent<EnemyCharacter>().attacker != null) GetComponent<EnemyCharacter>().attacker.EnemyDestroyed();
+
+            Destroy(parentObjectToDestroy);
+        }
+    }
+
+    public int GetHP()
+    {
+        return hp;
+    }
+
+
+    
+    // M A I N /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    void Start()
+    {
+        GetComponent<TextMesh>().text = hp.ToString();
+        settings = FindObjectOfType<Settings>();
+    }
+
+
+}
