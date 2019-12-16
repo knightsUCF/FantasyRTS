7750dfb1d8ab687ec4ef816eb7ee3a60e2d8cfde
KnightsUCF <peterk@knights.ucf.edu>
2019-12-16 18:23:10 -0500

    Create Settings.cs

---
+++ Code/Settings.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class Settings : MonoBehaviour
+{
+    
+    // S T A R T I N G   R E S O U R C E S ///////////////////////////////////////////////
+
+    public int startingGold = 4000;
+
+
+    // S T R U C T U R E   C O S T S /////////////////////////////////////////////////////
+
+    public int castleCost = 2000;
+    public int farmCost = 300;
+    public int barracksCost = 500;
+
+
+    // U N I T  C O S T S /////////////////////////////////////////////////////////////////
+
+    public int knightCost = 200;
+
+    // U N I T  H P ///////////////////////////////////////////////////////////////////////
+
+    public int knightHP = 100;
+
+    // W E A P O N  D A M A G E ///////////////////////////////////////////////////////////
+
+    public int swordDamage = 10;
+
+}
